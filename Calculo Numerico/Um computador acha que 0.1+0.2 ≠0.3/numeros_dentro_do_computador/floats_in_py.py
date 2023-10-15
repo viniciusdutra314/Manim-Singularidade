234bbeeab54ad98a066eb63eaf8427f0f33fd82f
viniciusdutra314 <dutravinisousa2@usp.br>
2023-10-15 08:50:12 -0300

    code snippets and everthing

--- Calculo Numerico/Um computador acha que 0.1+0.2 ≠0.3/numeros_dentro_do_computador/floats_in_py.py
+++ Calculo Numerico/Um computador acha que 0.1+0.2 ≠0.3/numeros_dentro_do_computador/floats_in_py.py
@@ -1,32 +1,41 @@
 import struct
 from rich import print
-import numpy as np
-format_string = '>f'
-my_variable = 10
-packed_data = struct.pack(format_string, my_variable)
-bytes_str = ''
-
-for byte in packed_data:
-    byte_str = format(byte, '08b')
-    bytes_str += byte_str
-
+from numpy import float16,float32,float64
+def number_to_bytes(number):
+    str_formats={float64:'d',float:'d',float32:'f',float16:'e'}
+    formatter=str_formats[type(number)]
+    bytes_str = ''
+    packed_data = struct.pack(f'>{formatter}', number)
+    for byte in packed_data:
+        byte_str = format(byte, '08b')
+        bytes_str += byte_str
+    return bytes_str
 def fractional_binary(binary:str) -> float:
-    value=np.float128(1)
+    value=1
     for position, byte in enumerate(binary):
         value+=int(byte)*(2**(-position-1))  
     return value
-sign_bit = bytes_str[0]
-exponent_bits = bytes_str[1:9]
-exponent=int(exponent_bits,2)-127
-fraction_bits = bytes_str[9:]
-fraction=fractional_binary(fraction_bits)
-print(f"{my_variable} em representação Float32 =")
-print(f"[red]{sign_bit}[/red][green]{exponent_bits}[/green][bright_yellow]{fraction_bits}[/bright_yellow]")
-print('')
-print(f"Sinal 1-bit: [red]{sign_bit}[/red] {'(positivo)' if sign_bit else 'negativo'}")
-print('')
-print(f"Expoente 8-bits: [green]{exponent_bits}= {exponent+127}-127=[/green]{exponent}")
-print('')
-print(f"Fracionário 23-bits [bright_yellow]{fraction_bits}[/bright_yellow] = {fraction}")
-print("----------------")
-print(f"{my_variable}=(Float-32)[bright_yellow]{fraction}[/bright_yellow]*(2^[green]{exponent}[/green])={fraction*np.power(2,exponent)}")
+def preaty_print_float(number,exact_representation):
+    #breakpoint()
+    bytes_str=number_to_bytes(number)
+    bits_info={float16:(16,5),float32:(32,8),float64:(64,11),float:(64,11)}
+    bits_total,bits_exp=bits_info[type(number)]
+    bits_mantissa=bits_total-bits_exp-1
+    sign_bit = bytes_str[0]
+    exp_bias=2**(bits_exp-1)-1
+    exponent=int(bytes_str[1:bits_exp+1],2)-exp_bias
+    binary_exponent=bytes_str[1:bits_exp+1]
+    mantissa = fractional_binary(bytes_str[bits_exp+1:])
+    binary_mantissa=bytes_str[bits_exp+1:]
+    print(f"{exact_representation} em representação Float{bits_total} =")
+    print(f"[red]{sign_bit}[/red][green]{binary_exponent}[/green][bright_yellow]{binary_mantissa}[/bright_yellow]")
+    print('')
+    print(f"Sinal 1-bit: [red]{sign_bit}[/red] {'(positivo)' if sign_bit else 'negativo'}")
+    print('')
+    print(f"Expoente {bits_exp}-bits: [green]{binary_exponent}= {exponent+exp_bias}-{exp_bias}=[/green]{exponent}")
+    print('')
+    print(f"Mantissa {bits_mantissa}-bits [bright_yellow]{bytes_str[bits_exp+1:]}[/bright_yellow] = {mantissa:.{bits_mantissa}f}")
+    print("----------------")
+    print(f"{exact_representation}(Float-{bits_total})=[bright_yellow]{mantissa:.{bits_mantissa}f}[/bright_yellow]*(2^[green]{exponent}[/green])={mantissa*2**exponent:.{bits_total}f}")
+x=float64(0.3)
+preaty_print_float(x,'0.3')
